b57981d7b1cafd39634a1052eb975bd24da2bcf6
Dorel Vaida <dorel@eloquentix.com>
2016-08-24 14:14:34 +0300

    Add the sleep

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -3,24 +3,18 @@ package com.example.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.os.SystemClock;
 
 public class Hello extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
-
-            String name = data.getString(0);
-            String message = "Hello, " + name;
+            int timeInMillis = data.getInt(0);
+            SystemClock.sleep(timeInMillis);
             callbackContext.success(message);
 
             return true;
 
-        } else {
-            
-            return false;
-
-        }
     }
 }
